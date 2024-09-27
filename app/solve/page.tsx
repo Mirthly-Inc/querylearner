7d1cab7d036837c4c8a8b750161510b8658ceef6
Anandprabhu530 <anandprabhu530@gmail.com>
2024-09-27 13:19:34 +0530

    Monacho editor added

--- app/solve/page.tsx
+++ app/solve/page.tsx
@@ -2,9 +2,11 @@
 
 import { useState } from "react";
 import ReactMarkdown from "react-markdown";
+import Editor from "@monaco-editor/react";
 
 export default function Solve() {
   const [data, setData] = useState("");
+  const [value, setValue] = useState("");
 
   const handleClick = async () => {
     console.log("Clicked");
@@ -12,34 +14,7 @@ export default function Solve() {
       method: "POST",
       body: JSON.stringify({
         problem: `Binary Search, Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.`,
-        code: `class Main {
-                  public static void main(String[] args){
-                      int[] arr = {5};
-                      int target = 5;
-                      int solution = search(arr,target);
-                      System.out.println(solution);
-                  }
-
-                  public static int search(int[] nums, int target) {
-                      if(target>nums[nums.length-1] || target<nums[0]){
-                          return -1;
-                      }
-                      int start = 0;
-                      int end = nums.length-1;
-                      while(start<end){
-                          int mid = start + (end - start)/2;
-                          if(nums[mid]==target){
-                              return mid;
-                          }
-                          if(nums[mid]>target){
-                              end = mid - 1;
-                          }else{
-                              start = mid+1;
-                          }
-                      }
-                      return -1;
-                  }
-                }`,
+        code: value,
         expected: "0",
       }),
     }).then((response) => response.json());
@@ -54,16 +29,25 @@ export default function Solve() {
   return (
     <div className="w-full rounded-lg bg-neutral-800 h-full flex-col">
       <div className="p-3 border-b border-white">Code</div>
-      <textarea className="h-fit w-full border-2 border-white text-black p-4" />
+      <Editor
+        height="80vh"
+        theme="vs-dark"
+        language="java"
+        value={value}
+        onChange={(value) => setValue(value)}
+        defaultValue={`\npublic class Main {\n\tpublic static void main(String[] args) {\n\t\tint[] arr = {5};\n\t\tint target = 5;\n\t\tSystem.out.println(search(nums,target));\n\t}
+          \n\tpublic static int search(int[] nums, int target) {\n\t\t//write your code here\n\n\t}\n}\n`}
+      />
       <button
-        onClick={handleClick}
+        // onClick={handleClick}
+        onClick={() => console.log(value)}
         className="border-2 border-white p-2 rounded-lg hover:bg-neutral-900"
       >
         Click to solve
       </button>
-      <ReactMarkdown className="text-white text-xl pt-10 border-2 border-white justify-center p-4">
+      {/* <ReactMarkdown className="text-white text-xl pt-10 border-2 border-white justify-center p-4">
         {data}
-      </ReactMarkdown>
+      </ReactMarkdown> */}
     </div>
   );
 }

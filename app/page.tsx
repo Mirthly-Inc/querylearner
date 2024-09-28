6658ed15cb2e31d3973c8292bf76ce1e8bc0ac17
Anandprabhu530 <anandprabhu530@gmail.com>
2024-09-28 21:42:36 +0530

    Demo done

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,12 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="w-full flex h-screen items-center flex-col gap-8 justify-center">
-      <div className="text-3xl font-semibold">Welcome to QueryLearner</div>
+    <div className="w-full font-sans flex h-screen items-center flex-col justify-center">
+      <div className="text-5xl font-semibold pb-4">Welcome to QueryLearner</div>
+      <div className="pb-8">
+        Master coding with the Socratic method. Level up your coding skills with
+        AI
+      </div>
       <Link
         href="/solve"
         className="p-2 px-4 bg-white text-black rounded-lg font-semibold cursor-pointer text-xl"
